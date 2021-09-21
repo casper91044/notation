4b858c32c7d24e1e7cadaa2782c55d1f215515d8
casper91044 <casper91044@mail.ru>
2021-09-21 11:27:14 +0300

    Component and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TodoList} from "./TodoList";
 
 function App() {
+
+    const task1 = [
+        {id: 1, title: "HTML&CSS", isDone: false},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: true}
+    ];
+
+    const task2 = [
+        {id: 1, title: "Milk", isDone: true},
+        {id: 2, title: "Sugar", isDone: true},
+        {id: 3, title: "Bread", isDone: false}
+    ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList title="What to Learn" tasks={task1}/>
+      <TodoList title="Product" tasks={task2}/>
     </div>
   );
 }
